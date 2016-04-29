b90b8c39c9950bd771f3386821fe6167271db3d9
Gustavo Fragoso <gbfragoso94@gmail.com>
2016-04-29 11:05:04 -0300

    Update pcode.h
    Change definition of pcode() adding a output file pointer.

--- pcode.h
+++ pcode.h
@@ -12,10 +12,12 @@ typedef struct{
 
 #endif
 
+#include<stdio.h>
+
 enum { LIT, OPR, LOD, STO, CAL, INT, JMP, JPC, WRT } instructionCode;
 enum { RTN, NEG, ADD, SUB, MUL, DIV, ODD, MOD, EQL, NEQ, LSS, LEQ, GTR, GEQ } operationCode;
 
-void pcode(Tinstruction inst[], int stack[]);
+void pcode(Tinstruction inst[], int stack[], FILE *output);
 int getBase(int level, int base, int stack[]);
 char* getInstructionName(int inst);
 
