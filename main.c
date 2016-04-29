13f292173a8422fdbe4618c1c681e7c58e9a778a
Gustavo Fragoso <gbfragoso94@gmail.com>
2016-04-29 11:07:55 -0300

    Update main.c

--- main.c
+++ main.c
@@ -19,61 +19,79 @@ int main (int argc, char **argv){
     int treeSize = 0;			// Number of values at input file
     string *btree;				// Dynamic allocated Binary Tree (Array of char*)
     
-    FILE *file;
+    FILE *input;
     
     if (argc == 3){
     	// Open file
     	string fileName = argv[1];
-    	file = fopen(fileName,"r");
+    	input = fopen(fileName,"r");
     	
     	// Reserve memory
     	treeSize = atoi(argv[2]);
     	btree = (string*) malloc(treeSize * sizeof(string));
     	
-    	if(file != NULL){
-    		int i = 0;
-    		char buffer[200];
-    		char expr[100]={""};
-    		string token = NULL;
+    	if(input != NULL){
     		
-    		printf("%s open ... Reading data\n", fileName);
-    		
-    		// Get input
-    		fgets(buffer, sizeof(buffer), file);
+    		FILE *output = fopen("output.txt", "w");
+	    	if(output!=NULL){
+	    		
+	    		int i = 0;
+	    		char buffer[200] = {""};
+	    		string expr = NULL;
+	    		string token = NULL;
+	    		
+	    		expr = (string) malloc(100*sizeof(string));
+	
+	    		// Get input
+	    		fgets(buffer, sizeof(buffer), input);
+				fprintf(output,"Running: %s %s %s\n", argv[0],argv[1],argv[2]);
+		    	fprintf(output,"Input: %s\n", buffer);
+		    	
+	    		// Tokenizing
+	    		token = strtok(buffer,",");
+	    		btree[0] = malloc (sizeof(token)+1);
+	    		strcpy(btree[0],token);
+	    		
+	    		while(token != NULL){
+	    			token = strtok(NULL,",");
+	    			if(token != NULL){
+	    				i++;
+	    				btree[i] = malloc (sizeof(token)+1);
+	    				strcpy(btree[i],token);	
+	    			}
+	    		}
+		    	
+		    	// B-tree -> Reverse Polish Notation
+		    	postOrder(btree,treeSize, 0, expr);
+		    	
+		    	fprintf(output,"Reverse polish notation: %s\n", expr);
+	
+		    	// Array of instructions
+			    Tinstruction * instructions = toInstruction(expr, treeSize);
+			    fprintf(output,"\nOutput:\n");
+			    fprintf(output,"%-10s %-15s %-15s %-15s %-15s %-15s %s\n","Inst","Level","Arg","Top","Counter","Base","Stack");
+			    pcode(instructions, stack, output);
 
-    		// Tokenizing
-    		token = strtok(buffer,",");
-    		btree[0] = malloc (sizeof(token)+1);
-    		strcpy(btree[0],token);
-    		
-    		while(token != NULL){
-    			token = strtok(NULL,",");
-    			if(token != NULL){
-    				i++;
-    				btree[i] = malloc (sizeof(token)+1);
-    				strcpy(btree[i],token);	
-    			}
-    		}
-    		
-	    	fclose(file);
-	    	printf("\nTranslating to reverse polish notation ...\n");
-	    	
-	    	// B-tree -> Reverse Polish Notation
-	    	postOrder(btree,treeSize, 0, expr);
-	    	
-	    	printf("Reverse polish notation: %s\n", expr);
-	    	
-	    	for(i = 0; i<treeSize; i++){
-	    		free(btree[i]);
+	    		// Free memory
+			    for(i = 0; i<treeSize; i++){
+		    		free(btree[i]);
+		    	}
+		    	free(btree);
+		    	free(expr);
+		    	
+		    	// Closing Files
+		    	fclose(input);
+	    		fclose(output);
+	    		
+	    	}else{
+	    		printf("Error when opening the output file.");
+	    		fclose(input);
 	    	}
-	    	free(btree);
-
-	    	// Array of instructions
-		    Tinstruction * instructions = toInstruction(expr, treeSize);
-		    printf("\nStarting the P-code Machine.\n");
-		    printf("%-10s %-15s %-15s %-15s %-15s %s\n","Inst","Level","Arg","Top","Counter","Stack");
-		    pcode(instructions, stack);
-    	}
+    	}else{
+	    	printf("Error when opening the input file.");
+	    }
+    }else{
+    	printf("Insuficient params");
     }
     return 0;
 }
